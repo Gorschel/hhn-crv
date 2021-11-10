88faef0041e76860aa0b5f5f91d31d781d6cf802
mrtnhaag <mrtnhaag@gmail.com>
2021-11-10 10:12:11 +0100

    NN file executable, should work data divided in stamped/ unstamped

--- src/mobilenet_tl.py
+++ src/mobilenet_tl.py
@@ -17,14 +17,46 @@ import pathlib
 
 from sklearn.model_selection import train_test_split
 
-
-
 IMAGE_SHAPE = (224, 224)
+
+data_dir = pathlib.Path(pathlib.Path.home(), 'PycharmProjects/hhn-crv/data')
+
+
+data_stamped = list(data_dir.glob('stamped_pics/*'))
+
+data_unstamped = list(data_dir.glob('unstamped_pics/*'))
+
+letters_images_dict = {
+  'stamped': list(data_dir.glob('stamped_pics/*')),
+  'unstamped': list(data_dir.glob('unstamped_pics/*')),
+}
+
+letters_labels_dict = {
+  'stamped': 0,
+  'unstamped': 1,
+}
+
+X, y = [], []
+
+for category, images in letters_images_dict.items():
+    for image in images:
+        img = cv2.imread(str(image))
+        resized_img = cv2.resize(img, IMAGE_SHAPE)
+        X.append(resized_img)
+        y.append(letters_labels_dict[category])
+
+X = np.array(X)
+y = np.array(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+X_train_scaled = X_train / 255
+X_test_scaled = X_test / 255
 
 feature_extractor_model = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4"
 pretrained_model_without_top_layer = hub.KerasLayer(feature_extractor_model, input_shape=(224, 224, 3), trainable=False)
 
-num_classes = 5
+num_classes = 2
 
 model = tf.keras.Sequential([
   pretrained_model_without_top_layer,
@@ -33,4 +65,11 @@ model = tf.keras.Sequential([
 
 model.summary()
 
+model.compile(
+  optimizer="adam",
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['acc'])
 
+model.fit(X_train_scaled, y_train, epochs=5)
+
+model.evaluate(X_test_scaled, y_test)
